713c9c4e55a52772b0c6d1a7061ce332c24c80e0
Dillon Morris <dmorris@elmstreettechnology.com>
2018-12-07 12:59:06 -0700

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import styled from 'styled-components'
+import Container from './Container'
+
+const Hero = () => {
+  return (
+    <Container>
+      <div>I am the hero</div>
+    </Container>
+  )
+}
+
+export default Hero
---
+++ src/templates/index.js
@@ -0,0 +1,17 @@
+// import { graphql } from 'gatsby'
+import React, { Component } from 'react'
+import Layout from '../components/Layout'
+import Home from '../components/Home'
+
+class Gems extends Component {
+  render() {
+    // const { data } = this.props
+    return (
+      <Layout>
+        <Home />
+      </Layout>
+    )
+  }
+}
+
+export default Gems
